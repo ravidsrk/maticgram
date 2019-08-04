1eff3c615d4cdb4cc84bc71858e2dabfc0e6173d
Ravindra Kumar <ravidsrk@gmail.com>
2019-08-04 09:15:06 +0530

    UI for Add to Patreon and Buy and Tip

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,40 @@
+/* eslint-disable brace-style */
+/* eslint-disable no-undef */
+/* eslint-disable quotes */
 /* eslint-env jquery, browser */
-$(document).ready(() => {
+$(document).ready(async () => {
+  // Modern dapp browsers...
+  const App = {};
+  let maticContract;
+  if (window.ethereum) {
+    App.web3Provider = window.ethereum;
+    try {
+      // Request account access
+      await window.ethereum.enable();
+    } catch (error) {
+      // User denied account access...
+      console.error('User denied account access');
+    }
+  }
 
-  // Place JavaScript code here...
+  // Legacy dapp browsers...
+  else if (window.web3) {
+    App.web3Provider = window.web3.currentProvider;
+  }
 
+  // If no injected web3 instance is detected, fall back to Ganache
+  else {
+    App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+  }
+  web3 = new Web3(App.web3Provider);
+
+  console.log(web3.version);
+
+  await initMaticgram();
+
+  function initMaticgram() {
+    maticContract = new web3.eth.Contract(contractAbi, contractAddress);
+
+    console.log(maticContract.methods.length);
+  }
 });
